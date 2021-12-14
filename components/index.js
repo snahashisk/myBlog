de8d5c878b580b6bd6d4c5046961e344aed2b211
snahashis <snahashiskanrar@gmail.com>
2021-12-14 12:43:30 +0530

    env update

--- components/index.js
+++ components/index.js
@@ -10,3 +10,4 @@ export { default as CommentsForm } from './CommentsForm';
 export { default as PostDetail } from './PostDetail';
 export { default as FeaturedPostCard } from './FeaturedPostCard';
 export { default as Loader } from './Loader';
+export { default as AdjacentPostCard } from './AdjacentPostCard';
